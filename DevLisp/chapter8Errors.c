0b92a06a6422b094aedc65d650a183f69af243a3
Pesara Amarasekera <2010pesara@gmail.com>
2018-12-31 16:53:10 -0500

    Added Chapter7 and Chapter8
    Chapter 7 has still got some bonus exercises to go through.
    Chapter 8 has just been started.

---
+++ DevLisp/chapter8Errors.c
@@ -0,0 +1,15 @@
+/*
+Pesara Amarasekera
+2018-12-31
+This file contains the error handling code for Maitri 
+Programming Language
+Watching for Heisenbugs (Mutating Bugs???)
+*/
+
+/*New lval Struct Declaration*/
+typedef struct{
+    int type; 
+    long num; 
+    int err;
+}lval;
+
